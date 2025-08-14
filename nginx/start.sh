553129aa69c1bb4493a3a51df37b9eaddbed90d1
kprimecity <kprimecity@yahoo.fr>
2025-08-14 05:49:32 +0200

    Update start.sh

--- nginx/start.sh
+++ nginx/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-envsubst < /env.conf.template > /env.conf
-mv /default.conf /etc/nginx/conf.d/${NGINX_SERVER_NAME}.conf
+envsubst < /etc/nginx/conf.d/env.conf.template > /etc/nginx/conf.d/env.conf
+mv /etc/nginx/conf.d/default.conf /etc/nginx/conf.d/${NGINX_SERVER_NAME}.conf
