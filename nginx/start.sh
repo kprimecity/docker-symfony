c9d49eb9d91cbfbeced4556954cdc2f0b12240f9
kprimecity <kprimecity@yahoo.fr>
2025-08-15 06:53:13 +0200

    Update start.sh

--- nginx/start.sh
+++ nginx/start.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-envsubst < /env.conf.template > /env.conf
-mv /default.conf /etc/nginx/conf.d/${NGINX_SERVER_NAME}.conf
+DEFAULT_CONF="/default.conf"
+ENV_CONF="/env.conf.template"
+
+if [ -f "$ENV_CONF" ]; then
+	# Put the user variable and rename it...
+    envsubst < /env.conf.template > /env.conf
+fi
+
+if [ -f "$DEFAULT_CONF" ]; then
+    # Move the default conf...
+	mv /default.conf /etc/nginx/conf.d/default.conf
+fi
 
 nginx -g "daemon off;"
