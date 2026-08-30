a2e23f2c1d9e304ea02aff57d40f7bf5e11424ee
kprimecity <kprimecity@yahoo.fr>
2026-08-30 02:28:42 -0500

    Update start.sh
    Clean up the code!

--- src/start.sh
+++ src/start.sh
@@ -3,50 +3,52 @@
 # Exit immediately if any command fails
 set -e
 
-PHP_INI="/custom-php.ini"
-OPCACHE="/custom-opcache.ini"
-WWW_PHP_FPM="/custom-www-php-fpm.conf"
-NGINX_CONF="/nginx.conf"
-DEFAULT_CONF="/default.conf"
+# Env
+PHP_INI="custom-php.ini"
+OPCACHE="custom-opcache.ini"
+WWW_PHP_FPM="custom-www-php-fpm.conf"
+NGINX_CONF="nginx.conf"
+DEFAULT_CONF="default.conf"
 INDEX_FILE_PATH="/var/www/html/public/index.php"
 
-if [ -f "$PHP_INI" ]; then
-    # Move the php ini file...
-    mv "$PHP_INI" /usr/local/etc/php/conf.d"$PHP_INI"
-    echo "Your custom PHP ini file is located at: '/usr/local/etc/php/conf.d$PHP_INI'."
+# Move the php ini file...
+if [ -f "/$PHP_INI" ]; then
+    mv "/$PHP_INI" /usr/local/etc/php/conf.d/"$PHP_INI"
+    echo "Your custom PHP ini file is located at: '/usr/local/etc/php/conf.d/$PHP_INI'."
 fi
 
-if [ -f "$OPCACHE" ]; then
-    # Move the php ini file...
-    mv "$OPCACHE" /usr/local/etc/php/conf.d"$OPCACHE"
-    echo "Your custom opcache ini file is located at: '/usr/local/etc/php/conf.d$OPCACHE'."
+# Move the OPCACHE file...
+if [ -f "/$OPCACHE" ]; then
+    mv "/$OPCACHE" /usr/local/etc/php/conf.d/"$OPCACHE"
+    echo "Your custom opcache ini file is located at: '/usr/local/etc/php/conf.d/$OPCACHE'."
 fi
 
-if [ -f "$WWW_PHP_FPM" ]; then
-    # Move the php ini file...
-    mv "$WWW_PHP_FPM" /etc/php/fpm/pool.d"$WWW_PHP_FPM"
-    echo "Your custom php-fpm pool file is located at: '/etc/php/fpm/pool.d$WWW_PHP_FPM'."
+# Move the WWW_PHP_FPM file...
+if [ -f "/$WWW_PHP_FPM" ]; then
+    mv "/$WWW_PHP_FPM" /usr/local/etc/php-fpm.d/"$WWW_PHP_FPM"
+    echo "Your custom php-fpm pool file is located at: '/usr/local/etc/php-fpm.d/$WWW_PHP_FPM'."
 fi
 
-if [ -f "$NGINX_CONF" ]; then
-    # Move Nginx default config
-	mv /nginx.conf /etc/nginx/nginx.conf
+# Move the NGINX_CONF file...
+if [ -f "/$NGINX_CONF" ]; then
+	mv "/$NGINX_CONF" /etc/nginx/"$NGINX_CONF"
     echo "Your Nginx config file is located at: '/etc/nginx/$NGINX_CONF'."
 fi
 
-if [ -f "$DEFAULT_CONF" ]; then
-    # Move Nginx default config
-	mv /default.conf /etc/nginx/http.d/default.conf
-    echo "Your Nginx Server config file is located at: '/etc/nginx/http.d$DEFAULT_CONF'."
+# Move the DEFAULT_CONF file...
+if [ -f "/$DEFAULT_CONF" ]; then
+	mv "/$DEFAULT_CONF" /etc/nginx/http.d/"$DEFAULT_CONF"
+    echo "Your Nginx Server config file is located at: '/etc/nginx/http.d/$DEFAULT_CONF'."
 fi
 
+# Download Symfony if not exist
 if [ -f "$INDEX_FILE_PATH" ]; then
     echo "Your index file is located at: '$INDEX_FILE_PATH'."
 else
     echo "Downloading Symfony framework..."
     
 	cd /var/www/html
-    COMPOSER_ALLOW_SUPERUSER=1 composer create-project symfony/skeleton .
+    COMPOSER_ALLOW_SUPERUSER=1 composer create-project symfony/skeleton .  --no-interaction
     COMPOSER_ALLOW_SUPERUSER=1 composer require webapp --no-interaction --prefer-dist
     COMPOSER_ALLOW_SUPERUSER=1 composer require norkunas/youtube-dl-php:dev-master --no-interaction --prefer-dist
 
@@ -54,25 +56,22 @@ else
     COMPOSER_ALLOW_SUPERUSER=1 composer install --no-interaction --prefer-dist --no-dev
 	COMPOSER_ALLOW_SUPERUSER=1 composer dump-autoload --no-interaction --no-dev --classmap-authoritative
     COMPOSER_ALLOW_SUPERUSER=1 composer update --no-interaction --prefer-dist
-
-    addgroup www-data
-    adduser -D -H -G www-data www-data
-    
-    chown -R www-data:www-data /var/html
-    chmod -R 775 /var/html
 fi
 
 # Clear and warm up the cache for the specific environment
-echo "Warming up application cache..."
-php bin/console cache:clear --no-interaction
-php bin/console cache:warmup --no-interaction
+if [ -d "/var/www/html/var/cache" ]; then
+    echo "🧹 Clearing old application cache..."
+	
+	cd /var/www/html
+    php bin/console cache:clear --no-interaction
+	php bin/console cache:warmup --no-interaction
+fi
 
-# Update and clear the cache
-apk update && apk upgrade 
-apk cache sync
+echo "🔒 Adjusting file permissions..."
+chown -R www-data:www-data /var/www
+chmod -R 775 /var/www
 
-# Start all services
-exec supervisord -c /etc/supervisord.conf
+echo "✅ Initialization complete. Handing over control to Supervisor..."
 
-# Execute the primary container command in Dockerfile(php-fpm)
+# Execute the primary container CMD in Dockerfile
 exec "$@"
